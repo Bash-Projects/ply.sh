77addbf9817a03f43d117d8297aac485d10d7f6b
wustho <benawiadha@gmail.com>
2020-04-25 07:15:12 +0700

    Sat 25 Apr 2020 07:15:12 AM WIB

--- ply.sh
+++ ply.sh
@@ -2,9 +2,11 @@
 
 # Edit these lines: {{{
 AUDIODIRS=(
-    "$HOME/audiodirs"
+#    "$HOME/audiodirs"
+    "$HOME/Music"
 )
-PLYLISTDIR="$HOME/audiodirs/plylists"
+#PLYLISTDIR="$HOME/audiodirs/plylists"
+PLYLISTDIR="$HOME/Music/plylists"
 PLAYERCMD="nvlc --no-video --loop --random"
 FINDERCMD="find ${AUDIODIRS[@]} -regextype posix-extended -regex "'.*(mp3|mp4|wav|flac|m4a|mkv)'
 # FINDERCMD="fdfind -I "'.*(mp3|mp4|wav|flac|m4a|mkv)'" ${AUDIODIRS[@]}"
@@ -19,7 +21,9 @@ Options:
     -l, --list        list contents of current DEFAULT playlist
     -b, --build       build DEFAULT playlist
     -s, --save        save DEFAULT playlist
-    -c, --choose       choose from saved playlists
+    -c, --choose      choose from saved playlists
+    -a, --append      append entries to chosen playlist
+    -r, --remove      remove entries from chosen playlist
 _EOF_
 }
 
@@ -44,7 +48,7 @@ if [ -n "$1" ]; then
                 ;;
             -b|--build)
                 # plstr=$($FINDERCMD ${AUDIODIRS[@]} | shuf | $FZFCMD)
-                plstr=$($FINDERCMD | shuf | fzf -m --height=100 -d '^.*/' --with-nth=2 --prompt="Add_to_plylist: ")
+                plstr=$($FINDERCMD | shuf | fzf -m --height=100 -d '^.*/' --with-nth=2 --prompt="Add to plylist: ")
                 [[ -z "$plstr" ]] && die "Canceled." || echo "$plstr" > "$plylist"
                 exit
                 ;;
@@ -63,6 +67,22 @@ if [ -n "$1" ]; then
             -d|--delete)
                 tmpstr=$(find "$PLYLISTDIR" -type f ! -name "plylist-default.m3u" -name "plylist-*.m3u" | fzf -m -d '^.*/plylist-' --prompt="Delete plylist: " --with-nth=2 --preview 'cat {} | while read line; do basename "$line"; done')
                 [[ -z "$tmpstr" ]] && die "Canceled." || set -f; ORIFS=$IFS; IFS=$'\n'; rm ${tmpstr[@]}; IFS=$ORIFS; set +f
+                exit
+                ;;
+            -a|--append)
+                tmpstr=$(find "$PLYLISTDIR" -type f -name "plylist-*.m3u" | fzf -d '^.*/plylist-' --prompt="Choose plylist to append: " --with-nth=2 --preview 'cat {} | while read line; do basename "$line"; done')
+                [[ -z "$tmpstr" ]] && die "Canceled." || chosenplylist="$tmpstr"
+                plstr=$($FINDERCMD | shuf | fzf -m --height=100 -d '^.*/' --with-nth=2 --prompt="Append to ${chosenplylist/*-/}: " --preview 'cat '"$chosenplylist"' | while read line; do basename "$line"; done')
+                [[ -z "$plstr" ]] && die "Canceled." || echo "$plstr" >> "$chosenplylist"
+                exit
+                ;;
+            -r|--remove)
+                tmpstr=$(find "$PLYLISTDIR" -type f -name "plylist-*.m3u" | fzf -d '^.*/plylist-' --prompt="Plylist to remove entries: " --with-nth=2 --preview 'cat {} | while read line; do basename "$line"; done')
+                [[ -z "$tmpstr" ]] && die "Canceled." || chosenplylist="$tmpstr"
+                cat "$chosenplylist" | fzf -m -d '^.*/' --with-nth=2 --prompt="Which entries to remove: " | while read line
+                do
+                    sed -i '/'"${line//\//\\\/}"'/d' "$chosenplylist"
+                done
                 exit
                 ;;
             *)
